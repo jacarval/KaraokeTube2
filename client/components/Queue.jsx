577f3da2d627ba0c8d9e180d8db1a70e4cbfc798
Jamal Carvalho <jamal.a.carvalho@gmail.com>
2016-03-16 12:45:15 -0400

    queue shows previous tracks, new layout

--- client/components/Queue.jsx
+++ client/components/Queue.jsx
@@ -9,13 +9,40 @@ export class Queue extends Component {
   	return this.props.queue || []
   }
 
+  getPrevious() {
+    return this.props.previous || []
+  }
+
+  getPlaying() {
+    return this.props.playing ? [this.props.playing] : []
+  }
+
+  componentDidMount() {
+    document.getElementById("now_playing").scrollIntoView()
+  }
+
   render() {
   	return (
   		<div className="queue">
         <div className="container">
-          <div className="row align-middle">
+            {this.getPrevious().map(song => {
+              return <Previous key={song.get('id')}
+                title={song.get('title')}
+                user={song.get('user')}
+                description={song.get('description')}
+                thumburl={song.get('thumburl')}
+              />
+            })}
+            {this.getPlaying().map(song => {
+              return <Playing key={song.get('id')}
+                title={song.get('title')}
+                user={song.get('user')}
+                description={song.get('description')}
+                thumburl={song.get('thumburl')}
+              />
+            })}
             {this.getQueue().map(song => {
-              return <Card key={song.get('id')}
+              return <Next key={song.get('id')}
                 title={song.get('title')}
                 user={song.get('user')}
                 description={song.get('description')}
@@ -25,11 +52,69 @@ export class Queue extends Component {
                 handlePlayNow={e => this.props.playNow(song)}
               />
             })}
-          </div>
         </div>
   		</div>
   	)
   }
+}
+
+const Playing = (props) => {
+  return <div id="now_playing" className="media-object stack-for-small">
+    <div className="media-object-section">
+      <div className="thumbnail">
+        <img src={props.thumburl}/>
+      </div>
+    </div>
+    <div className="media-object-section main-section">
+      <h4>{props.user}</h4>
+      <p>{props.title}</p>
+          <div className="action">
+      <div className="expanded button-group">
+        <a className="button success">Now Playing</a>
+      </div>
+    </div>
+    </div>
+  </div>
+}
+
+const Previous = (props) => {
+  return <div className="media-object stack-for-small">
+    <div className="media-object-section">
+      <div className="thumbnail">
+        <img src={props.thumburl}/>
+      </div>
+    </div>
+    <div className="media-object-section main-section">
+      <h4>{props.user}</h4>
+      <p>{props.title}</p>
+          <div className="action">
+      <div className="expanded button-group">
+        <a className="button disabled">Played</a>
+      </div>
+    </div>
+    </div>
+  </div>
+}
+
+const Next = (props) => {
+  return <div className="media-object stack-for-small">
+    <div className="media-object-section">
+      <div className="thumbnail">
+        <img src={props.thumburl}/>
+      </div>
+    </div>
+    <div className="media-object-section main-section">
+      <h4>{props.user}</h4>
+      <p>{props.title}</p>
+          <div className="action">
+      <div className="expanded button-group">
+        <a className="button" onClick={props.handlePlayNow}><i className="fa fa-play"/> Now</a>
+        <a className="button" onClick={props.handlePlayNext}><i className="fa fa-play"/> Next</a>
+        <a className="button" onClick={props.handleRemove}><i className="fa fa-remove"/> Remove</a>
+      </div>
+    </div>
+    </div>
+  </div>
 }
 
 const Card = (props) => {
@@ -56,6 +141,8 @@ const Card = (props) => {
 
 function mapStateToProps(state){
 	return {
+    playing: state.get('playing'),
+    previous: state.get('previous'),
 		queue: state.get('queue')
 	}
 }
